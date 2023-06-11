4115afebf63a455a569c2baecdf54eeaeb421afa
RaDiO-AcTiVe8970 <62706964+RaDiO-AcTiVe8970@users.noreply.github.com>
2023-06-11 21:45:20 +0600

    Company

---
+++ Company_Site/Company_Site/Controllers/CompanyController.cs
@@ -0,0 +1,63 @@
+﻿using Company_Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Company_Site.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            ViewBag.Bio = "MediaTek is an IP design and software company. It creates chips that are at the heart of mobile computing around the world. With solutions for smartphones, tablets, PCs, connected TVs, set-top boxes, automotives and M2M (M2M), MediaTek helps billions of consumers around the world connect, communicate and experience more in their daily lives.";
+            ViewBag.Mission = "MediaTek’s mission is to provide the best, most cost-effective IC solutions that enable breakthroughs in ubiquitous connectivity and multimedia experiences. MediaTek is a fabless semiconductor company with 8,000+ employees, including 4,000+ R&D engineers and 90+ offices worldwide. The company is headquartered in Taiwan and has offices in mainland China, Singapore, India, U.S., Japan, Korea, Denmark, England, Sweden and Dubai.";
+            ViewBag.Services = "MediaTek’s mission is to provide the best, most cost-effective IC solutions that enable breakthroughs in ubiquitous connectivity and multimedia experiences.";
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            var mem1 = new Members {
+                Name = "Rick Tsai",
+                Department = "Software",
+                Position = "System Engineer",
+                Bio = "Rick Tsai is a system engineer at MediaTek. He has been working at MediaTek for 5 years. He is a graduate of the University of California, Berkeley."
+            };
+
+            var mem2 = new Members
+            {
+                Name = "John Smith",
+                Department = "Software",
+                Position = "System Engineer",
+                Bio = "John Smith is a system engineer at MediaTek. He has been working at MediaTek for 15 years. He is a graduate of the University of California, Berkeley."
+            };
+            var mem3 = new Members {
+                Name="Jane Doe",
+                Department="IC Engineer",
+                Position="IC Engineer",
+                Bio="Jane Doe is an IC engineer at MediaTek. She has been working at MediaTek for 8 years. She is a graduate of the University of California, Berkeley."
+            };
+            var mem4 = new Members {
+                Name="Kaiser Fahim",
+                Department="IC Engineer",
+                Position="IC Engineer",
+                Bio="Kaiser Fahim is an IC engineer at MediaTek. He has been working at MediaTek for 5 years. He is a graduate of the BRAC University, Dhaka."
+            };
+            Members[] teams = new Members[] { mem1, mem2, mem3, mem4 };
+            return View(teams);
+        }
+
+        public ActionResult Contact() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Company_Site/Company_Site/Models/Members.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Company_Site.Models
+{
+    public class Members
+    {
+        public string Name { get; set; }   
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string Bio { get; set; }
+    }
+}
