eb781940d7ee7d981c015f06a4c04c8c14fd241c
RaDiO-AcTiVe8970 <62706964+RaDiO-AcTiVe8970@users.noreply.github.com>
2023-06-18 15:03:08 +0600

    lab task 1

---
+++ lab_task/lab_task/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using lab_task.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult test()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Resgistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Resgistration(Register reg)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("test");
+            }
+            return View(reg);
+        }
+    }
+}
